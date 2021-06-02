8b49e897f2fbe30ecd9c4cbea678e2e3ae1fe8f3
KazmaArakaki <twi1ightmessenger1988@gmail.com>
2021-06-02 13:55:48 +0900

    言語一覧取得用APIエンドポイントを追加

---
+++ src/Controller/Api/LanguagesController.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Controller\Api;
+
+use App\Exception\Exception as AppException;
+use Cake\Log\Log;
+
+class LanguagesController extends ApiController {
+  public function initialize(): void {
+    parent::initialize();
+  }
+
+  public function index() {
+    $languagesData = $this->Languages->find()
+        ->order([
+          'name' => 'asc',
+        ])
+        ->map(function ($language) {
+          return [
+            'short_key' => $language['short_key'],
+            'name' => h($language['name']),
+          ];
+        })
+        ->toList();
+
+    $this->responseData['data'] = [
+      'languages' => $languagesData,
+    ];
+  }
+}
+
